1d37cf132e3d5baf19ecf6ab639f251e22bede70
Rônero Gomes Cavalcante Júnior <ronero.gomes@cepal.rede>
2025-12-10 14:34:45 -0300

    versao prod

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Criamos uma instância do axios com uma configuração base
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'http://10.1.11.135:5001/api',
   withCredentials: true
 });
 
